bc4073994b5feee4b47f56f1e378661464b47b14
andreluis034 <andrebrandao@sapo.pt>
2017-03-13 17:45:06 +0000

    Monomio function

---
+++ monomio.c
@@ -0,0 +1,44 @@
+#include "monomio.h"
+#include <stdlib.h>
+
+MonomioVariable* CreateMonomioVariable(char variable, int expoente)
+{
+    MonomioVariable* monomioVariable = malloc(sizeof(MonomioVariable));
+    monomioVariable->Variable = variable;
+    monomioVariable->Expoente = expoente;
+    return monomioVariable;
+}
+
+MonomioVariables* createVariableList(MonomioVariable* monomioVariable)
+{
+    MonomioVariables* variableList = malloc(sizeof(MonomioVariable));
+    variableList->Value = monomioVariable;
+    variableList->Next = NULL;
+    return variableList;
+}
+
+MonomioVariable* addMonomioVariable(MonomioVariables* monomioVariable, MonomioVariable* toPrepend)
+{
+    MonomioVariable* variableList = createVariableList(toPrepend);
+    variableList->Next = monomioVariable;
+    return variableList;
+}
+
+Monomio* CreateConstant(double constant)
+{
+    Monomio* monomio = malloc(sizeof(Monomio));
+    monomio->Coeficiente = constant;
+    monomio->Variables = NULL;
+    return monomio;
+}
+
+Monomio* CreateMonomio(double coeficient, MonomioVariables* addMonomioVariable)
+{
+    Monomio* monomio = CreateConstant(coeficient);
+    monomio->Variables = addMonomioVariable;
+}
+
+Monomio* MonomioAddVariable(Monomio* monomio, MonomioVariable* variable)
+{
+    monomio->Variables = addMonomioVariable(monomio->Variables, variable);
+}
--- monomio.h
+++ monomio.h
@@ -1,21 +1,22 @@
 #include "list.h"
-typedef struct _monomioVariavel
+typedef struct _monomioVariable
 {
-    char variavel;
-    int expoente;
-} MonomioVariavel;
+    char Variable;
+    int Expoente;
+} MonomioVariable;
 
-typedef List MonomioVariaveis;
+typedef List MonomioVariables;
 
 typedef struct _monomio
 {
-	double coeficiente;
-    MonomioVariaveis variaveis;
+	double Coeficient;
+    MonomioVariables Variables;
 } Monomio;
 
-MonomioVariavel* CreateMonomioVariable(char variable, int expoente);
-MonomioVariavel* addMonomioVariable(MonomioVariavel* monomioVariavel, MonomioVariavel* variavelPrep);
+MonomioVariable* CreateMonomioVariable(char variable, int expoente);
+MonomioVariables* createVariableList(MonomioVariable* monomioVariable);
+MonomioVariable* addMonomioVariable(MonomioVariables* monomioVariable, MonomioVariable* VariablePrep);
 
 Monomio* CreateConstant(double constant);
-Monomio* CreateMonomio(double coeficient);
-Monomio* MonomioAddVariable(Monomio* addMonomioVariable);
+Monomio* CreateMonomio(double coeficient, MonomioVariables* addMonomioVariable);
+Monomio* MonomioAddVariable(Monomio* monomio, MonomioVariable* variable);
